c54d458d5d35b148fa8db5039c7da34b03e2a666
orstar626-eng <orstar626@gmail.com>
2026-08-14 13:55:53 +0300

    Add files via upload

---
+++ OrxaProvider/build.gradle.kts
@@ -0,0 +1,10 @@
+version = 1
+
+cloudstream {
+    description = "ORXA Arabic movies and series provider"
+    authors = listOf("ORXA")
+    status = 1
+    tvTypes = listOf("Movie", "TvSeries")
+    language = "ar"
+    iconUrl = "https://orxa.vercel.app/icon.png"
+}
