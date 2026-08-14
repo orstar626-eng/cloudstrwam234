c91231e4cd67468270b75d74fe8a351585c3abc9
orstar626-eng <orstar626@gmail.com>
2026-08-14 13:31:37 +0300

    Add files via upload

---
+++ OrxaProvider/src/main/kotlin/com/orxa/OrxaPlugin.kt
@@ -0,0 +1,12 @@
+package com.orxa
+
+import android.content.Context
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+
+@CloudstreamPlugin
+class OrxaPlugin : Plugin() {
+    override fun load(context: Context) {
+        registerMainAPI(OrxaProvider())
+    }
+}
